38815f8640e91703742bc29d2eb7ade5f7c4d35f
cdnmix <122172916+cdnmix@users.noreply.github.com>
2023-01-11 13:11:18 -0300

    Create init.js

---
+++ hot/init.js
@@ -0,0 +1,264 @@
+{
+
+
+"Hetero": [{
+"title": "Novidades",
+"ico_title": "img/ico_flame.png",
+"subtitle": "Vídeos em alta recém adicionados",
+"url": "https://www.xvideos.com/new/1",
+
+"rootviewtype": 0,
+"viewtypelayout": 0,
+
+"space": false,
+"especialurl": false,
+"especialtitle": false
+},
+{
+"title": "Vídeos em alta",
+"ico_title": "img/ico_graficy.png",
+"subtitle": "Os videos mais assistidos de <?php 
+$oldLocale = setlocale(LC_TIME, 'pt_BR');
+echo strtolower(strftime('%B',time()));
+setlocale(LC_TIME, $oldLocale);
+ ?>",
+"url": "https://www.xvideos.com/best/2022-<?php 
+$month = date('m', strtotime("last month")); echo $month; ?>/",
+
+"rootviewtype": 0,
+"viewtypelayout": 0,
+
+"space": false,
+"especialurl": false,
+"especialtitle": false
+},
+{
+"title": "Canais populares",
+"ico_title": "",
+"subtitle": "Melhores canais porno semanais",
+"url": "https://www.xvideos.com/channels-index/from/worldwide/",
+
+"rootviewtype": 0,
+"viewtypelayout": 1,
+
+"space": true,
+"especialurl": true,
+"especialtitle": false
+},
+{
+"title": "Vídeos amadores recentes",
+"ico_title": "",
+"subtitle": "",
+"url": "https://www.xvideos.com/c/Amateur-65/",
+
+"rootviewtype": 0,
+"viewtypelayout": 0,
+
+"space": false,
+"especialurl": false,
+"especialtitle": false
+},
+{
+"title": "Novos vídeos verificados",
+"ico_title": "img/ico_verify_white.png",
+"subtitle": "",
+"url": "https://www.xvideos.com/verified/videos/",
+
+"rootviewtype": 0,
+"viewtypelayout": 0,
+
+"space": true,
+"especialurl": false,
+"especialtitle": false
+},
+{
+"title": "Estrelas pornô",
+"ico_title": "img/ico_stars.png",
+"subtitle": "Atrizes hetero populares do momento",
+"url": "https://www.xvideos.com/pornstars-index/from/worldwide/3months/",
+
+"rootviewtype": 0,
+"viewtypelayout": 2,
+
+"space": false,
+"especialurl": true,
+"especialtitle": true
+},
+{
+"title": "Vídeos brasileiros",
+"ico_title": "img/ico_bra.png",
+"subtitle": "Vídeos em português adicionados recentemente",
+"url": "https://www.xvideos.com/lang/portugues/",
+
+"rootviewtype": 0,
+"viewtypelayout": 0,
+
+"space": false,
+"especialurl": false,
+"especialtitle": false
+}],
+
+
+
+
+"Trans": [{
+"title": "Novidades",
+"ico_title": "",
+"subtitle": "Vídeos em alta recém adicionados",
+"url": "https://www.xvideos.com/shemale/",
+"rootviewtype": 0,
+"viewtypelayout": 0,
+"space": false,
+"especialurl": false
+},
+{
+"title": "Vídeos em alta",
+"ico_title": "",
+"subtitle": "Os videos mais assistidos de <?php 
+$oldLocale = setlocale(LC_TIME, 'pt_BR');
+echo strtolower(strftime('%B',time()));
+setlocale(LC_TIME, $oldLocale);
+ ?>",
+"url": "https://www.xvideos.com/best-of-shemale/2022-<?php 
+$month = date('m', strtotime("last month")); echo $month; ?>/",
+"rootviewtype": 0,
+"viewtypelayout": 0,
+"space": false,
+"especialurl": false
+},
+{
+"title": "Canais populares",
+"ico_title": "",
+"subtitle": "Melhores canais porno semanais",
+"url": "https://www.xvideos.com/channels-trans/from/worldwide/",
+"rootviewtype": 0,
+"viewtypelayout": 0,
+"space": true,
+"especialurl": true
+},
+{
+"title": "Vídeos amadores recentes",
+"ico_title": "",
+"subtitle": "",
+"url": "https://www.xvideos.com/c/Shemale_Amateur-225/",
+"rootviewtype": 0,
+"viewtypelayout": 0,
+"space": false,
+"especialurl": false
+},
+{
+"title": "Novos vídeos verificados",
+"ico_title": "",
+"subtitle": "",
+"url": "https://www.xvideos.com/verified-shemale/videos/",
+"rootviewtype": 0,
+"viewtypelayout": 0,
+"space": true,
+"especialurl": false
+},
+{
+"title": "Estrelas pornô",
+"ico_title": "",
+"subtitle": "Atrizes trans populares do momento",
+"url": "https://www.xvideos.com/pornstars-index/trans/from/worldwide/3months/",
+"rootviewtype": 0,
+"viewtypelayout": 0,
+"space": false,
+"especialurl": true
+},
+{
+"title": "Vídeos brasileiros",
+"ico_title": "",
+"subtitle": "Vídeos em português adicionados recentemente",
+"url": "https://www.xvideos.com/?k=brazil&typef=shemale&p=",
+"rootviewtype": 0,
+"viewtypelayout": 0,
+"space": false,
+"especialurl": false
+}],
+
+
+
+
+
+"Gay": [{
+"title": "Novidades",
+"ico_title": "",
+"subtitle": "Vídeos em alta recém adicionados",
+"url": "https://www.xvideos.com/gay/",
+"rootviewtype": 0,
+"viewtypelayout": 0,
+"space": false,
+"especialurl": false
+},
+{
+"title": "Vídeos em alta",
+"ico_title": "",
+"subtitle": "Os videos mais assistidos de <?php 
+$oldLocale = setlocale(LC_TIME, 'pt_BR');
+echo strtolower(strftime('%B',time()));
+setlocale(LC_TIME, $oldLocale);
+ ?>",
+"url": "https://www.xvideos.com/best-of-gay/2022-<?php 
+$month = date('m', strtotime("last month")); echo $month; ?>/",
+"rootviewtype": 0,
+"viewtypelayout": 0,
+"space": false,
+"especialurl": false
+},
+{
+"title": "Canais populares",
+"ico_title": "",
+"subtitle": "Melhores canais porno semanais",
+"url": "https://www.xvideos.com/channels-gay/from/worldwide/",
+"rootviewtype": 0,
+"viewtypelayout": 0,
+"space": true,
+"especialurl": true
+},
+{
+"title": "Vídeos amadores recentes",
+"ico_title": "",
+"subtitle": "",
+"url": "https://www.xvideos.com/c/Gay_amateur-93/",
+"rootviewtype": 0,
+"viewtypelayout": 0,
+"space": false,
+"especialurl": false
+},
+{
+"title": "Novos vídeos verificados",
+"ico_title": "",
+"subtitle": "",
+"url": "https://www.xvideos.com/verified-gay/videos/",
+"rootviewtype": 0,
+"viewtypelayout": 0,
+"space": true,
+"especialurl": false
+},
+{
+"title": "Estrelas pornô",
+"ico_title": "",
+"subtitle": "Atores gays populares do momento",
+"url": "https://www.xvideos.com/pornstars-index/gay/from/worldwide/3months/",
+"rootviewtype": 0,
+"viewtypelayout": 0,
+"space": false,
+"especialurl": true
+},
+{
+"title": "Vídeos brasileiros",
+"ico_title": "",
+"subtitle": "Vídeos em português adicionados recentemente",
+"url": "https://www.xvideos.com/?k=brazil&typef=gay&p=",
+"rootviewtype": 0,
+"viewtypelayout": 0,
+"space": false,
+"especialurl": false
+}]
+
+
+
+
+
+}
